ae21765f4f21ad689e7f51c6f058199178f69a2c
Damien Robertson <robertsondamien@gmail.com>
2017-12-18 01:02:35 -0500

    Moved struct related functions into struct impl

--- rust/recursive_circus/src/main.rs
+++ rust/recursive_circus/src/main.rs
@@ -11,58 +11,60 @@ struct Node {
     children: Vec<String>,
 }
 
+impl Node {
+    fn find_tree_root(tree_nodes: &Vec<Node>) -> String {
+        let mut start: String = match tree_nodes.iter().cloned().nth(0) {
+            Some(n) => n.parent,
+            None => panic!("FAILURE : EMPTY TREE NODE VECTOR"),
+        };
+
+        loop {
+            match tree_nodes.iter().cloned()
+                .filter(|n| n.children.contains(&start))
+                .map(|n| n.parent).next() {
+                    Some(p) => start = p,
+                    None => break start,
+            }
+        }
+    }
+
+    fn parse_node_parent(line: &str) -> String {
+        match line.split_whitespace().nth(0) {
+            Some(p) => String::from(p),
+            None => panic!("FAILURE : PARSE NODE > PARENT"),
+        }
+    }
+
+    fn parse_node_weight(line: &str) -> u32 {
+        match line.split_whitespace().nth(1) {
+            Some(p) => p.trim_matches(|c| c == '(' || c == ')').parse().unwrap(),
+            None => panic!("FAILURE : PARSE NODE > WEIGHT"),
+        }
+    }
+
+    fn parse_node_children(line: &str) -> Vec<String> {
+        match line.split("->").nth(1) {
+            Some(p) => p.split(", ").map(|x| String::from(x.trim())).collect(),
+            None => Vec::new(),
+        }
+    }
+}
+
 fn main() {
 
     let contents: String = open_file_read_contents();
 
     let tree_nodes: Vec<Node> = contents.lines().map(|line| {
         Node {
-            parent: parse_node_parent(line),
-            weight: parse_node_weight(line),
-            children: parse_node_children(line),
+            parent: Node::parse_node_parent(line),
+            weight: Node::parse_node_weight(line),
+            children: Node::parse_node_children(line),
         }
     }).collect();
 
-    let tree_root: String = find_tree_root(&tree_nodes);
+    let tree_root: String = Node::find_tree_root(&tree_nodes);
     println!("TREE ROOT: {:?}", &tree_root);
 
-}
-
-fn find_tree_root(tree_nodes: &Vec<Node>) -> String {
-    let mut start: String = match tree_nodes.iter().cloned().nth(0) {
-        Some(n) => n.parent,
-        None => panic!("FAILURE : EMPTY FOREST"),
-    };
-
-    loop {
-        match tree_nodes.iter().cloned()
-            .filter(|n| n.children.contains(&start))
-            .map(|n| n.parent).next() {
-                Some(p) => start = p,
-                None => break start,
-        }
-    }
-}
-
-fn parse_node_parent(line: &str) -> String {
-    match line.split_whitespace().nth(0) {
-        Some(p) => String::from(p),
-        None => panic!("FAILURE : PARSE NODE > PARENT"),
-    }
-}
-
-fn parse_node_weight(line: &str) -> u32 {
-    match line.split_whitespace().nth(1) {
-        Some(p) => p.trim_matches(|c| c == '(' || c == ')').parse().unwrap(),
-        None => panic!("FAILURE : PARSE NODE > WEIGHT"),
-    }
-}
-
-fn parse_node_children(line: &str) -> Vec<String> {
-    match line.split("->").nth(1) {
-        Some(p) => p.split(", ").map(|x| String::from(x.trim())).collect(),
-        None => Vec::new(),
-    }
 }
 
 fn open_file_read_contents() -> String {
