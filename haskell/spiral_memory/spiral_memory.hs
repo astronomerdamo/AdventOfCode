1109d7bd233ab87f8bc42861c02959fa241f1782
Damien Robertson <robertsondamien@gmail.com>
2018-01-21 09:12:06 -0500

    Update README for day 3

--- haskell/spiral_memory/spiral_memory.hs
+++ haskell/spiral_memory/spiral_memory.hs
@@ -10,8 +10,22 @@ mdist n =
   abs (((n - 1) `mod` (2 * r)) - r) + r
     where r = nring n
 
+rcoords :: Int -> [Int]
+rcoords r =
+  [-(r-1)..r] ++ replicate (2*r) r ++ reverse [-r..(r-1)] ++ replicate (2*r) (-1*r)
+
+crec :: Int -> ((Int, Int), Int)
+crec 1 = ((0,0),1)
+crec n = (head $ zip x y, n)
+    where r = nring n
+          coords = rcoords r
+          x = drop (2*r) coords ++ take (2*r) coords
+          y = coords
+
 main = do
   args <- getArgs
   let n = read . head $ args :: Int
+  let alist = map crec [1..n]
+  print alist
   -- Find the manhatten distance
   printf "Manhattan Distance: %d\n" (mdist n)
