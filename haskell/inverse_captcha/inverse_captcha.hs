73834fcb6c3d50d56334279df75a2afc512e6297
Damien Robertson <robertsondamien@gmail.com>
2018-02-25 00:36:08 -0500

    use less variables

--- haskell/inverse_captcha/inverse_captcha.hs
+++ haskell/inverse_captcha/inverse_captcha.hs
@@ -2,16 +2,25 @@ import System.Environment
 import Data.Char (digitToInt)
 import Data.List (transpose)
 
+captcha :: [String] -> [Int]
+captcha args =
+  map digitToInt $ head args
+
+cmd :: [String] -> String
+cmd args =
+  head $ tail args
+
 shiftn :: String -> [Int] -> Int
 shiftn cmd captcha
   | cmd == "wrap" = 1
   | cmd == "half" = length captcha `div` 2
   | otherwise = 1
 
+captchaPairs :: [String] -> [[Int]]
+captchaPairs args =
+  transpose [captcha args, drop n (captcha args) ++ take n (captcha args)]
+    where n = shiftn (cmd args) (captcha args)
+
 main = do
   args <- getArgs
-  let captcha = map digitToInt $ head args
-  let cmd = head $ tail args
-  let n = shiftn cmd captcha
-  let captcha_pairs = transpose [captcha, drop n captcha ++ take n captcha]
-  print (sum[head p | p <- captcha_pairs, head p == last p])
+  print (sum[head p | p <- captchaPairs args, head p == last p])
