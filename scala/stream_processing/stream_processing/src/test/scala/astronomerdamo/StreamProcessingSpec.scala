fb858f9c8a2333dee3b4617fd20f1a87580099a8
Damien Robertson <robertsondamien@gmail.com>
2018-03-03 12:53:45 -0500

    part b complete

--- scala/stream_processing/stream_processing/src/test/scala/astronomerdamo/StreamProcessingSpec.scala
+++
@@ -1,9 +0,0 @@
-package astronomerdamo
-
-import org.scalatest._
-
-class HelloSpec extends FlatSpec with Matchers {
-  "The Hello object" should "say hello" in {
-    Hello.greeting shouldEqual "hello"
-  }
-}
