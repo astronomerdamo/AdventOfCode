2f59f52d19494e763a4f050726934061b390b2be
Damien Robertson <robertsondamien@gmail.com>
2018-03-12 22:12:10 -0400

    Rust fmt on memory reallocation

--- rust/memory_reallocation/src/main.rs
+++ rust/memory_reallocation/src/main.rs
@@ -2,21 +2,25 @@ use std::fs;
 use std::env;
 use std::io::Read;
 
-#[derive(Debug)]
-#[derive(Clone)]
-#[derive(PartialOrd, Ord, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialOrd, Ord, PartialEq, Eq)]
 struct Bank {
     index: u8,
     blocks: u16,
 }
 
 fn find_max_bank_by_index(mem_banks: &Vec<Bank>) -> (u8, u16) {
-    let max_bank = mem_banks.iter().cloned().fold(Bank {index: 0, blocks: 0}, |mut acc, s| {
-        if (acc.blocks == s.blocks && acc.index > s.index) || acc.blocks < s.blocks {
-            acc = s;
-        }
-        acc
-    });
+    let max_bank = mem_banks.iter().cloned().fold(
+        Bank {
+            index: 0,
+            blocks: 0,
+        },
+        |mut acc, s| {
+            if (acc.blocks == s.blocks && acc.index > s.index) || acc.blocks < s.blocks {
+                acc = s;
+            }
+            acc
+        },
+    );
     (max_bank.index, max_bank.blocks)
 }
 
@@ -25,17 +29,15 @@ fn reset_max_bank_blocks(mem_banks: &mut Vec<Bank>, max_bank_index: &u8) {
         if bank.index == *max_bank_index {
             (*bank).blocks = 0;
         }
-    };
+    }
 }
 
 fn reallocate_memory_banks(mut mem_banks: &mut Vec<Bank>) {
-
     let (mut max_bank_index, max_bank_blocks) = find_max_bank_by_index(&mem_banks);
     let mut bank_index = max_bank_index.clone();
     reset_max_bank_blocks(&mut mem_banks, &mut max_bank_index);
 
     for _ in 0..max_bank_blocks {
-
         if (bank_index + 1) >= (mem_banks.len() as u8) {
             bank_index = 0;
         } else {
@@ -56,12 +58,16 @@ fn main() {
     let mut debug_count = 0u32;
 
     let (loop_count, loop_incidence) = loop {
-
         reallocate_memory_banks(&mut memory);
 
         debug_count += 1;
 
-        let matched_memory_state = match memory_states.iter().cloned().filter(|x| x.1 == memory).next() {
+        let matched_memory_state = match memory_states
+            .iter()
+            .cloned()
+            .filter(|x| x.1 == memory)
+            .next()
+        {
             Some(state) => (true, state.0),
             None => (false, 0u32),
         };
@@ -74,7 +80,10 @@ fn main() {
     };
 
     println!("Number of memory bank moves {:?}", &loop_count);
-    println!("Reallocation moves between loop boundries {:?}", &loop_count - &loop_incidence);
+    println!(
+        "Reallocation moves between loop boundries {:?}",
+        &loop_count - &loop_incidence
+    );
 }
 
 fn parse_memory_banks() -> Vec<Bank> {
