699d14a150f4d5d1a70224a5d8b21a3e42ff4064
Damien Robertson <robertsondamien@gmail.com>
2018-04-21 23:10:06 -0400

    Add option to run day 2

--- rust_revamp/rust/src/utils/req_utils.rs
+++ rust_revamp/rust/src/utils/req_utils.rs
@@ -3,18 +3,21 @@ use solutions;
 /// Request enum handles the possible requested solutions by the user.
 pub enum Request {
     Day1,
+    Day2,
 }
 
 impl Request {
     fn solution_runner(self) -> () {
         match self {
             Request::Day1 => solutions::day1::main(),
+            Request::Day2 => solutions::day2::main(),
         }
     }
 
     pub fn run_request(request_arg: &str) -> () {
         let solution_request = match request_arg {
             "1" => Request::Day1,
+            "2" => Request::Day2,
             _ => panic!("bad choice"),
         };
         solution_request.solution_runner();
