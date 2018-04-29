c27776b94daa10fb269082796aca105330eb63f1
Damien Robertson <robertsondamien@gmail.com>
2018-04-29 17:54:11 -0400

    Update day 8 solution

--- rust_revamp/rust/src/solutions/day8.rs
+++ rust_revamp/rust/src/solutions/day8.rs
@@ -1,8 +1,7 @@
-use std::fs;
-use std::env;
-use std::io::Read;
 use std::str::FromStr;
 use std::collections::HashMap;
+use std::path::Path;
+use utils;
 
 #[derive(Clone)]
 struct Instruction {
@@ -57,63 +56,36 @@ impl Instruction {
             .last().unwrap()
         ).expect("Cannot cast predicate argument to int")
     }
-
-    fn parse_instructions() -> Vec<Instruction> {
-        let mut instructions: Vec<Instruction> = Vec::new();
-        for (i, raw_instruction) in open_file_read_contents().lines().enumerate() {
-            instructions.push(
-                Instruction {
-                    index: i,
-                    register: Instruction::parse_register(raw_instruction),
-                    incrementor: Instruction::parse_incrementor(raw_instruction),
-                    predicate_actor: Instruction::parse_predicate_actor(raw_instruction),
-                    predicate_operator: Instruction::parse_predicate_operator(raw_instruction),
-                    predicate_argument: Instruction::prase_predicate_argument(raw_instruction),
-                }
-            );
-        };
-        instructions
-    }
-
 }
 
-fn main() {
-    let instructions: Vec<Instruction> = Instruction::parse_instructions();
-    let mut registers: HashMap<String, i64> = initialise_registers(&instructions);
-    let mut max_process_register: i64 = 0;
+fn load_raw_input() -> String {
+    let path = Path::new("./inputs/day8.txt");
+    println!("Loading inputs file: {:?}", &path);
+    utils::file_utils::read_inputs(path)
+}
 
-    for i in 0..instructions.len() {
-        let mut borrow_register = registers.to_owned();
-        let instruction: Instruction = get_instruction(&instructions, &i);
-        let register_value = registers.entry(instruction.register.to_owned()).or_insert(0);
-
-        if evaluate_predicate(&mut borrow_register, &instruction) {
-            let new_value = *register_value + instruction.incrementor;
-            max_process_register = if new_value > max_process_register {new_value} else {max_process_register};
-            *register_value = new_value;
-        }
+fn parse_input() -> Vec<Instruction> {
+    let mut instructions: Vec<Instruction> = Vec::new();
+    for (i, raw_instruction) in load_raw_input().lines().enumerate() {
+        instructions.push(
+            Instruction {
+                index: i,
+                register: Instruction::parse_register(raw_instruction),
+                incrementor: Instruction::parse_incrementor(raw_instruction),
+                predicate_actor: Instruction::parse_predicate_actor(raw_instruction),
+                predicate_operator: Instruction::parse_predicate_operator(raw_instruction),
+                predicate_argument: Instruction::prase_predicate_argument(raw_instruction),
+            }
+        );
     };
-
-    // Part A
-    let max_end_register = match registers.values().max() {
-        Some(x) => x,
-        None => panic!("Empty register: If you are seeing this, something has gone terribly wrong."),
-    };
-    println!("Largest end of instruction register value: {:?}", max_end_register);
-
-    //Part B
-    println!("Largest instruction register value: {:?}", max_process_register);
+    instructions.sort_by(|a, b| a.index.cmp(&b.index));
+    instructions
 }
 
-fn get_instruction(instructions: &[Instruction], i: &usize) -> Instruction {
-    instructions.iter().cloned().find(|instruction| instruction.index == *i).unwrap()
-}
-
-fn evaluate_predicate(registers: &mut HashMap<String, i64>, instruction: &Instruction) -> bool {
+fn evaluate_predicate(predicate_actor: i64, instruction: &Instruction) -> bool {
     let operator = instruction.predicate_operator.as_str();
     let predicate_argument = instruction.predicate_argument;
-    let predicate_actor = registers.entry(instruction.predicate_actor.to_owned()).or_insert(0).to_owned();
-    let predicate: bool = match operator {
+    match operator {
         "==" => predicate_actor == predicate_argument,
         "!=" => predicate_actor != predicate_argument,
         ">=" => predicate_actor >= predicate_argument,
@@ -121,28 +93,86 @@ fn evaluate_predicate(registers: &mut HashMap<String, i64>, instruction: &Instru
         ">" => predicate_actor > predicate_argument,
         "<" => predicate_actor < predicate_argument,
         _ => panic!("Unknown operator"),
-    };
-    predicate
+    }
 }
 
-fn initialise_registers(instructions: &[Instruction]) -> HashMap<String, i64> {
+fn run_instructions(instructions: &[Instruction]) -> (HashMap<String, i64>, i64) {
     let mut registers: HashMap<String, i64> = HashMap::new();
+    let mut max_register_value: i64 = 0;
     for instruction in instructions {
-        registers.insert(instruction.register.to_owned(), 0);
-    };
-    registers
+        if evaluate_predicate(registers.entry(instruction.predicate_actor.to_owned()).or_insert(0).clone(), instruction) {
+            let value = registers.entry(instruction.register.to_owned()).or_insert(0);
+            let new_value = *value + instruction.incrementor;
+            *value = new_value;
+            max_register_value = if new_value > max_register_value {new_value} else {max_register_value};
+        };
+    }
+    (registers, max_register_value)
 }
 
-fn open_file_read_contents() -> String {
-    let mut file = match fs::File::open(env::args().nth(1).expect("FAILURE : FILE PATH")) {
-        Ok(f) => f,
-        Err(e) => panic!("FAILURE : OPEN FILE {}", e),
-    };
+fn run_a(res: &(HashMap<String, i64>, i64)) -> i64 {
+    res.0.values().max().unwrap().to_owned()
+}
 
-    let mut buffer = String::new();
-    match file.read_to_string(&mut buffer) {
-        Ok(s) => s,
-        Err(e) => panic!("FAILURE : READ FILE {}", e),
-    };
-    buffer
+fn run_b(res: &(HashMap<String, i64>, i64)) -> i64 {
+    res.1
+}
+
+pub fn main() {
+    let input: Vec<Instruction> = parse_input();
+    let res = run_instructions(&input);    
+    println!("Day 8a: {:?}", &run_a(&res));
+    println!("Day 8a: {:?}", &run_b(&res));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_instructions() -> Vec<Instruction> {
+        vec![
+            Instruction {
+                index: 1,
+                register: "b".to_string(),
+                incrementor: 5,
+                predicate_actor: "a".to_string(),
+                predicate_operator: ">".to_string(),
+                predicate_argument: 1,
+            }, Instruction {
+                index: 2,
+                register: "a".to_string(),
+                incrementor: 1,
+                predicate_actor: "b".to_string(),
+                predicate_operator: "<".to_string(),
+                predicate_argument: 5,
+            }, Instruction {
+                index: 3,
+                register: "c".to_string(),
+                incrementor: 10,
+                predicate_actor: "a".to_string(),
+                predicate_operator: ">=".to_string(),
+                predicate_argument: 1,
+            }, Instruction {
+                index: 4,
+                register: "c".to_string(),
+                incrementor: -20,
+                predicate_actor: "c".to_string(),
+                predicate_operator: "==".to_string(),
+                predicate_argument: 10,
+            },
+        ]
+    }
+
+    #[test]
+    fn test_run_a() {
+        let test_res = run_instructions(&test_instructions());
+        assert_eq!(run_a(&test_res), 1);
+    }
+
+    #[test]
+    fn test_run_b() {
+        let test_res = run_instructions(&test_instructions());
+        assert_eq!(run_b(&test_res), 10);
+    }
+
 }
