d32f024f2b800353a6d9a8602a653ce2b81f59a0
Damien Robertson <robertsondamien@gmail.com>
2018-12-06 16:20:15 -0500

    Wip part 2

--- rust/src/solutions/year2018day1.rs
+++ rust/src/solutions/year2018day1.rs
@@ -20,9 +20,29 @@ fn run_a(input: &[isize]) -> isize {
         .sum()
 }
 
+fn run_b(input: &[isize]) -> isize {
+    let mut frequency_loop_not_found: bool = true;
+    let mut frequencies_seen: Vec<isize>;
+    let mut index: usize = 0;
+    let mut frequency_sum: isize = 0;
+    while frequency_loop_not_found {
+        let value = input.get(index).unwrap();
+        frequency_sum += value;
+        println!("{:?}", &value);
+        println!("{:?}", &frequency_sum);
+        index += 1;
+        
+        if index > 10 {
+            frequency_loop_not_found = false;
+        }
+    }
+    1
+}
+
 pub fn main() {
     let input: Vec<isize> = parse_input();
     println!("Year 2018 Day 1a: {:?}", &run_a(&input));
+    println!("Year 2018 Day 2a: {:?}", &run_b(&input));
 }
 
 #[cfg(test)]
@@ -35,5 +55,13 @@ mod tests {
         assert_eq!(run_a(&vec![1, 1, -2]), 0);
         assert_eq!(run_a(&vec![-1, -2, -3]), -6);
         assert_eq!(run_a(&vec![1, -2, 3, 1]), 3);
+    }
+
+    #[test]
+    fn test_run_b() {
+        assert_eq!(run_b(&vec![1, -1]), 0);
+        // assert_eq!(run_b(&vec![3, 3, 4, -2, -4 ]), 10);
+        // assert_eq!(run_b(&vec![-6, 3, 8, 5, -6]), 5);
+        // assert_eq!(run_b(&vec![7, 7, -2, -7, -4]), 14);
     }
 }
